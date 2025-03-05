9533960c09389cdb558a162ed58d7db2c14e9eda
Andy Wright <atwright147@gmail.com>
2025-03-05 18:04:05 +0000

    fix windows issues

--- files-windows.go
+++ files-windows.go
@@ -11,22 +11,16 @@ import (
 	"path/filepath"
 )
 
-//go:embed assets/windows/ExifTool.exe
+//go:embed assets/windows/*
 var exiftoolWindowsFS embed.FS
 
 func listExiftoolFiles() {
-	exiftoolFilesFS, err := fs.Sub(exiftoolWindowsFS, "exiftool_files")
-	if err != nil {
-		fmt.Printf("Failed to get exiftool_files sub FS: %v\n", err)
-		return
-	}
-
-	err = fs.WalkDir(exiftoolFilesFS, ".", func(path string, d fs.DirEntry, err error) error {
+	fmt.Println("Listing embedded files:")
+	err := fs.WalkDir(exiftoolWindowsFS, ".", func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
-
-		fmt.Println(path)
+		fmt.Printf("Found embedded file: %s\n", path)
 		return nil
 	})
 	if err != nil {
@@ -35,16 +29,59 @@ func listExiftoolFiles() {
 }
 
 func extractPlatformSpecificExiftool(tempDir string) (string, error) {
-	exiftoolName := "exiftool_windows.exe"
-	exiftoolPath := filepath.Join(tempDir, exiftoolName)
-	exiftoolData, err := exiftoolWindowsFS.ReadFile(exiftoolName)
+	exiftoolName := "ExifTool.exe"
+	exiftoolFilesDir := "assets/windows" // Use forward slashes for embedded filesystem
+
+	exiftoolPath := filepath.Join(tempDir, filepath.FromSlash(exiftoolFilesDir), exiftoolName)
+
+	exiftoolFilesFS, err := fs.Sub(exiftoolWindowsFS, exiftoolFilesDir)
 	if err != nil {
-		return "", fmt.Errorf("failed to read embedded exiftool: %v", err)
+		return "", fmt.Errorf("failed to get exiftool_files sub FS: %v", err)
 	}
 
-	err = os.WriteFile(exiftoolPath, exiftoolData, 0755)
+	err = fs.WalkDir(exiftoolFilesFS, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			fmt.Printf("Error walking through directory: %v\n", err)
+			return err
+		}
+
+		if d.IsDir() {
+			// fmt.Printf("Skipping directory: %s\n", path)
+			return nil
+		}
+
+		destPath := filepath.Join(tempDir, filepath.FromSlash(exiftoolFilesDir), filepath.FromSlash(path))
+		destDir := filepath.Dir(destPath)
+
+		if _, err := os.Stat(destDir); os.IsNotExist(err) {
+			err = os.MkdirAll(destDir, 0755)
+			if err != nil {
+				fmt.Printf("Failed to create directory: %v\n", err)
+				return err
+			}
+		}
+
+		fileData, err := fs.ReadFile(exiftoolFilesFS, path)
+		if err != nil {
+			fmt.Printf("Failed to read file: %v\n", err)
+			return err
+		}
+
+		err = os.WriteFile(destPath, fileData, 0644)
+		if err != nil {
+			fmt.Printf("Failed to write file: %v\n", err)
+			return err
+		}
+
+		return nil
+	})
 	if err != nil {
-		return "", fmt.Errorf("failed to write exiftool to disk: %v", err)
+		return "", fmt.Errorf("failed to extract exiftool_files: %v", err)
+	}
+
+	err = os.Chmod(exiftoolPath, 0755)
+	if err != nil {
+		return "", fmt.Errorf("failed to make exiftool executable: %v", err)
 	}
 
 	return exiftoolPath, nil
